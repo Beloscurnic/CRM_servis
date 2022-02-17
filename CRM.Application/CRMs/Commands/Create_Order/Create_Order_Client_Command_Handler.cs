e251fd7bf325b7aa0f0ef11c421c25c41433909b
belos <belos@MSI>
2022-02-17 20:26:49 +0200

    Внедрение зависемостей

---
+++ CRM.Application/CRMs/Commands/Create_Order/Create_Order_Client_Command_Handler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CRM.Domain;
+using MediatR;
+using CRM.Application.Interfaces;
+
+namespace CRM.Application.CRMs.Commands.Create_Order
+{
+  public  class Create_Order_Client_Command_Handler
+        :IRequestHandler<Create_Order_Client_Command, Guid>
+    {
+        private readonly ICRM_DbContext _dbContext;
+        public Create_Order_Client_Command_Handler(ICRM_DbContext dbContext) =>
+             _dbContext = dbContext;
+        public async Task <Guid> Handle(Create_Order_Client_Command request,
+            CancellationToken cancellationToken)
+        {
+            var order = new Order_Client
+            {
+                ID_Personnel = request.ID_Personnel,
+                ID_Client = request.ID_Client,
+                Name_Client = request.Name_Client,
+                LastName_Client = request.LastName_Client,
+                Email_Client = request.Email_Client,
+                Type_technology = request.Type_technology,
+                Model_technology = request.Model_technology,
+                Status_order = request.Status_order,
+                ID_Order = Guid.NewGuid(),
+                Receipt_date = DateTime.Now,
+                Issue_date = null
+            };
+            await _dbContext.Order_Clients.AddAsync(order, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return order.ID_Order;
+        }
+    }
+}
---
+++ CRM.Domain/Order_Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CRM.Domain
+{
+  public  class Order_Client
+    {
+        public Guid ID_Order { get; set; }
+        public Guid ID_Client { get; set; }
+        public Guid ID_Personnel { get; set; }
+        public string Name_Client { get; set; }
+        public string LastName_Client { get; set; }
+        public string Email_Client { get; set; }
+        public string Type_technology { get; set; }
+        
+        public string Model_technology { get; set; }
+        public string Status_order { get; set; }
+        public DateTime Receipt_date { get; set; }
+        public DateTime? Issue_date { get; set; }
+    }
+}
