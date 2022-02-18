eab7d0742238e4796371df8f88a0aa724aca8806
belos <belos@MSI>
2022-02-18 10:33:52 +0200

    Внедрение зависемостей

---
+++ CRM.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using CRM.Application.Interfaces;
+
+namespace CRM.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<CRM_DbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<ICRM_DbContext>(provider =>
+                provider.GetService<CRM_DbContext>());
+            return services;
+        }
+    }
+}
